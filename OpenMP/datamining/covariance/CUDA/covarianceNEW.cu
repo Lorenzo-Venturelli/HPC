582c17f70bb01290e542721677e1928792f808cc
unknown <matteopintaudi@gmail.com>
2022-12-10 18:23:32 +0100

    Correct Exercise

--- OpenMP/datamining/covariance/CUDA/covarianceNEW.cu
+++ OpenMP/datamining/covariance/CUDA/covarianceNEW.cu
@@ -14,6 +14,8 @@
 #define BLOCK_SIZE 32
 #endif
 
+#define LARGE_DATASET
+
 #define gpuErrchk(ans)                        \
     {                                         \
         gpuAssert((ans), __FILE__, __LINE__); \
@@ -83,35 +85,29 @@ void covariance(
 { 
   
   int i, j, j1, j2;
-
-	*float_n = 1.2; 
-   
+	*float_n = 1.2;    
    
 	for (i = 0; i < m; i++){
 		for (j = 0; j < n; j++){
    
 			data_correct[i * m + j] = ((double)i*j) / m;
       }
-   }
-  
+   }  
 
 	for (j = 0; j < m; j++)
 	{
 		mean_correct[j] = 0.0;
 
 		for (i = 0; i < n; i++)
-			mean_correct[j] += data_correct[j * m + i];
+			mean_correct[j] += data_correct[i * m + j];
 
 		mean_correct[j] /= *float_n;
 	}
  
- 
-
 	for (i = 0; i < n; i++)    
 		for (j = 0; j < m; j++)
 			data_correct[i * n + j] -= mean_correct[j];
-      
-  
+       
 
 #pragma omp parallel num_threads(4)
 {
@@ -135,27 +131,50 @@ void covariance(
 
 /* Main computational kernel. The whole function will be timed,
 	 including the call and return. */
-__global__ static void kernel_covariance(
+__global__ void kernel_covariance(
 	int m, 
 	int n, 
 	double float_n, 
-	double *data, 
-	double *symmat, 
-	double *correct,
-	double *mean
+	double *d_data, 
+	double *d_symmat,
+	double *d_mean
 	)
 {
-	int row = blockIdx.x * blockDim.x + threadIdx.x;
-	int column = blockIdx.y * blockDim.y + threadIdx.y;
-
-	if(row > m || column > n) {
-		return;
+	int row = threadIdx.x + blockIdx.x * blockDim.x;
+	int column = threadIdx.y + blockIdx.y * blockDim.y;
+ 
+  d_mean[column] = 0;
+	if(row < m && column < n) {
+ 
+    atomicadd(&d_mean[column], d_data[row * n + column]);
+  	//d_mean[column] = d_mean[column] + d_data[row * n + column];  	
+    d_mean[column] = d_mean[column] / float_n;  
+    
+    d_data[row * n + column] -= d_mean[column];
+    
+    /*for(int j = row; j < m; j = j + 1){
+		  d_symmat[row * m + j] = 0.0;
+		  atomicadd(&d_symmat[row * m + j], d_symmat[row * m + j] + d_data[column * n + row] * d_data[column * n + j]);
+		  //symmat[j * m + row] = symmat[row * m + j];
+	  }*/
+     
+     //__syncthreads();
+     
+   double sum = 0.0;    
+   if(column>=row){
+      d_symmat[row * m + column] = 0.0;           
+      for(int j = 0; j < m; j++){           
+        sum +=  d_data[j * m + row] * d_data[j * n + column];
+	    }
+      atomicadd(&d_symmat[row * m + column], sum);      
+      d_symmat[column * m + row] = d_symmat[row * m + column];
+    }    
 	}
-
+ 
+  //__syncthreads();
+  
 	/* Determine mean of column vectors of input data matrix */ //[row * m + column]
-	mean[row] = 0.0;
-	mean[row] = mean[row] + data[row * n + column];
-	mean[row] = mean[row] / float_n;
+	
 
 	/*for (j = 0; j < m; j++)
 	{
@@ -171,8 +190,8 @@ __global__ static void kernel_covariance(
 	}*/
 			
 	/* Center the column vectors. */
-	data[column * n + row] = data[column * n + row] - mean[row];
-	__syncthreads();
+	//d_data[column * n + row] = d_data[column * n + row] - d_mean[column];
+	//__syncthreads();
 
 	/*
 	for (i = 0; i < n; i++)    
@@ -182,12 +201,12 @@ __global__ static void kernel_covariance(
 
 			
 	/* Calculate the m * m covariance matrix. */
-	for(int j = row; j < m; j = j + 1)
-	{
-		symmat[row * m + j] = 0.0;
-		atomicadd(&symmat[row * m + j], symmat[row * m + j] + data[column * n + row] * data[column * n + j]);
-		symmat[j * m + row] = symmat[row * m + j];
-	}
+	//for(int j = row; j < m; j = j + 1)
+	//{
+		//symmat[row * m + j] = 0.0;
+		//atomicadd(&symmat[row * m + j], symmat[row * m + j] + data[column * n + row] * data[column * n + j]);
+		//symmat[j * m + row] = symmat[row * m + j];
+	//}
  
  
  //for (int i = 0; i<m*n; i++){
@@ -209,7 +228,7 @@ __global__ static void kernel_covariance(
 		}
 	}
 	*/
-	return;
+	//return;
 }
 
 int main(int argc, char *argv[])
@@ -221,13 +240,11 @@ int main(int argc, char *argv[])
 
 	/* Retrieve problem size. */
 	int n = N;
-	int m = M;
-  
+	int m = M; 
  
-
 	/* Variable declaration/allocation. */
 	double float_n;
-	double *data, *symmat, *correct, *mean;
+	double *data, *symmat, *mean;
  
   double *data_correct, *symmat_correct, *mean_correct;
     if (argc > 1){
@@ -243,7 +260,22 @@ int main(int argc, char *argv[])
         printf("error: memory allocation for 'x'\n");
         iret = -1;
     }
-    if (NULL == (mean_correct = (double *)malloc(sizeof(*mean_correct) * n * m)))
+    if (NULL == (mean_correct = (double *)malloc(sizeof(*mean_correct) * m)))
+    {
+        printf("error: memory allocation for 'x'\n");
+        iret = -1;
+    }
+    if (NULL == (data = (double *)malloc(sizeof(*data) * N * N)))
+    {
+        printf("error: memory allocation for 'x'\n");
+        iret = -1;
+    }
+    if (NULL == (symmat = (double *)malloc(sizeof(*symmat) * n * m)))
+    {
+        printf("error: memory allocation for 'x'\n");
+        iret = -1;
+    }
+    if (NULL == (mean = (double *)malloc(sizeof(*mean) * m)))
     {
         printf("error: memory allocation for 'x'\n");
         iret = -1;
@@ -254,65 +286,73 @@ int main(int argc, char *argv[])
         free(data_correct);
         free(symmat_correct);
         free(mean_correct);
+        free(data);
+        free(symmat);
+        free(mean);
+        
         exit(EXIT_FAILURE);
-    }
+    }   
    
- 
+  clock_gettime(CLOCK_REALTIME, rt + 0); 
   covariance(m, n, &float_n, data_correct, symmat_correct, mean_correct);
+  clock_gettime(CLOCK_REALTIME, rt + 1);
+  wt = (rt[1].tv_sec - rt[0].tv_sec) + 1.0e-9 * (rt[1].tv_nsec - rt[0].tv_nsec);
+  printf("Covariance (HOST): %9.3f sec %9.1f GFLOPS\n", wt, 2.0 * n * n * n / (1.0e9 * wt));
   
 	/* Allocate CUDA structures */
 	gpuErrchk(cudaMallocManaged((void **)&data, sizeof(double) * N * M));
-	gpuErrchk(cudaMallocManaged((void **)&symmat, sizeof(double) * M * M));
-	gpuErrchk(cudaMallocManaged((void **)&correct, sizeof(double) * M * M));
-	gpuErrchk(cudaMallocManaged((void **)&mean, sizeof(double) * M));
+	//gpuErrchk(cudaMallocManaged((void **)&symmat, sizeof(double) * M * M));
+	//gpuErrchk(cudaMallocManaged((void **)&correct, sizeof(double) * M * M));
+	//gpuErrchk(cudaMallocManaged((void **)&mean, sizeof(double) * n));
     
-
-	// Calculate Block size
-	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-  dim3 dimGrid((m + BLOCK_SIZE - 1) / BLOCK_SIZE, (n + BLOCK_SIZE - 1) / BLOCK_SIZE);
-
 	/* Initialize array(s). */
 	init_array(m, n, &float_n, data);
  
   double *d_symmat, *d_mean, *d_data;
- gpuErrchk(cudaMalloc((void **)&d_symmat, sizeof(double) * n * n));
- gpuErrchk(cudaMalloc((void **)&d_mean, sizeof(double) * n * n));
- gpuErrchk(cudaMalloc((void **)&d_data, sizeof(double) * n * n));
- gpuErrchk(cudaMemcpy(d_symmat, symmat, sizeof(double) * n * n, cudaMemcpyHostToDevice));
- gpuErrchk(cudaMemcpy(d_mean, mean, sizeof(double) * n, cudaMemcpyHostToDevice));
- gpuErrchk(cudaMemcpy(d_data, data, sizeof(double) * n * n, cudaMemcpyHostToDevice));
-
+  gpuErrchk(cudaMalloc((void **)&d_symmat, sizeof(double) * n * n));
+  gpuErrchk(cudaMalloc((void **)&d_mean, sizeof(double) * n));
+  gpuErrchk(cudaMalloc((void **)&d_data, sizeof(double) * n * n));
+  
+  clock_gettime(CLOCK_REALTIME, rt + 0);
+  /* Initialize array(s). */
+	init_array(m, n, &float_n, data);  
+  
+  gpuErrchk(cudaMemcpy(d_symmat, symmat, sizeof(double) * n * n, cudaMemcpyHostToDevice));
+  gpuErrchk(cudaMemcpy(d_mean, mean, sizeof(double) * n, cudaMemcpyHostToDevice));
+  gpuErrchk(cudaMemcpy(d_data, data, sizeof(double) * n * n, cudaMemcpyHostToDevice));
+  
 	/* Run kernel. */
-	clock_gettime(CLOCK_REALTIME, rt + 0);
-	kernel_covariance<<<dimGrid, dimBlock>>>(m, n, float_n, d_data, d_symmat, correct, d_mean);
+ 
+  //Calculate Block size
+  dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 dimGrid((m + BLOCK_SIZE - 1) / BLOCK_SIZE, (n + BLOCK_SIZE - 1) / BLOCK_SIZE); 
+ 
+	kernel_covariance<<<dimGrid, dimBlock>>>(m, n, float_n, d_data, d_symmat, d_mean);
 	gpuErrchk(cudaPeekAtLastError());
 	clock_gettime(CLOCK_REALTIME, rt + 1);
 	wt = (rt[1].tv_sec - rt[0].tv_sec) + 1.0e-9 * (rt[1].tv_nsec - rt[0].tv_nsec);
-  printf("GEMM (GPU): %9.3f sec %9.1f GFLOPS\n", wt, 2.0 * n * n * n / (1.0e9 * wt));
-   
+  printf("Covariance (GPU): %9.3f sec %9.1f GFLOPS\n", wt, 2.0 * n * n * n / (1.0e9 * wt));   
   
-  gpuErrchk(cudaMemcpy(symmat, d_symmat, sizeof(float) * n * n, cudaMemcpyDeviceToHost));
-  gpuErrchk(cudaMemcpy(data, d_data, sizeof(float) * n * n, cudaMemcpyDeviceToHost));
-  gpuErrchk(cudaMemcpy(mean, d_mean, sizeof(float) * n, cudaMemcpyDeviceToHost));
+  gpuErrchk(cudaMemcpy(symmat, d_symmat, sizeof(double) * n * n, cudaMemcpyDeviceToHost));
+  gpuErrchk(cudaMemcpy(data, d_data, sizeof(double) * n * n, cudaMemcpyDeviceToHost));
+  gpuErrchk(cudaMemcpy(mean, d_mean, sizeof(double) * n, cudaMemcpyDeviceToHost));
   
-  
-  
-  for (int i = 0; i<m; i++){
-    printf("%.2f\n", mean_correct[i]);
-      //if (correct[i][j] != symmat[i][j]) {ok++;}
-  }
+  int counter = 0;
+  for (int i = 0; i<m*n; i++){
+    //if(data[i] != data_correct[i]) counter += 1;
+    //printf("%.2f\n", symmat[i]);
+    //printf("%.2f\n", symmat_correct[i]);
+    }
+    
+  //printf("%d", m*n);  
     
 	gpuErrchk(cudaFree(data));
-	gpuErrchk(cudaFree(symmat));
   gpuErrchk(cudaFree(d_symmat));
   gpuErrchk(cudaFree(d_mean));
-  gpuErrchk(cudaFree(d_data));
-	gpuErrchk(cudaFree(correct));
-	gpuErrchk(cudaFree(mean));
-  
+  gpuErrchk(cudaFree(d_data));  
  
     
-	//cudaDeviceReset();
+	cudaDeviceReset();
 
 	//kernel_covariance(m, n, float_n, data, symmat, correct, mean);
 
